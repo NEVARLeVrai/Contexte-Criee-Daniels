ab30fdfee7a95083cb8a49f2973caba376c079d4
Daniels SOARES <danielo-24@hotmail.com>
2024-12-11 16:43:22 +0100

    v1.6
    mise a jour de l'accueil et du css

--- application/views/Menu1.php
+++ application/views/Menu1.php
@@ -24,47 +24,74 @@ session_start();
         <!-- Menu latéral -->
         <div class="sidebar">
             <div class="sidebar-header">
-                <h2>Menu</h2>
+                <i class="fas fa-bars"></i><h2>Menu</h2>
                 <button class="close-btn">
                     <i class="fas fa-times"></i>
                 </button>
             </div>
             <nav class="sidebar-nav">
-                <a href="<?php echo site_url('welcome/contenu/Commandes');?>">
-                    <button class="sidebar-item">
-                        <i class="fas fa-shopping-cart"></i> Commandes
+
+
+                <a href="<?php echo site_url('welcome/contenu/Accueil');?>">
+                    <button class="sidebar-item deco-button" style="margin-top: 50px; ">
+                        <i class="fas fa-home"></i> Accueil
+                    </button>
+                </a>
+
+                
+                <a href="<?php echo site_url('welcome/contenu/Connexion');?>">
+                        <button class="sidebar-item deco-button">
+                            <i class="fas fa-user"></i> Compte
                     </button>
                 </a>
 
                 <?php if (isset($_SESSION) && $_SESSION): ?>
                 <a href="<?php echo site_url('welcome/contenu/Deconnexion');?>">
-                    <button class="sidebar-item">
+                    <button class="sidebar-item deco-button" style="margin-bottom: 50px;">
                         <i class="fas fa-sign-out-alt"></i> Deconnexion
                     </button>
                 </a>
                 <?php endif; ?>
+
+
+                <a href="<?php echo site_url('welcome/contenu/Annonces');?>">
+                    <button class="sidebar-item deco-button"  style="margin-top: 50px; margin-bottom: 50px;">
+                        <i class="fas fa-bullhorn"></i> Annonces
+                    </button>
+                </a>
+
+
+
+
+                <a href="<?php echo site_url('welcome/contenu/Connexion');?>">
+                    <button class="sidebar-item deco-button">
+                        <i class="fas fa-file-alt"></i> Mentions
+                    </button>
+                </a>
+
+                <a href="<?php echo site_url('welcome/contenu/Connexion');?>">
+                    <button class="sidebar-item deco-button">
+                        <i class="fas fa-envelope"></i> Contact
+                    </button>
+                </a>
+
+                <a href="<?php echo site_url('welcome/contenu/Connexion');?>">
+                    <button class="sidebar-item deco-button">
+                        <i class="fas fa-clock"></i> Horaires
+                    </button>
+                </a>
             </nav>
         </div>
         <header>
             <nav id="navbar">
-                <form id="navForm">  
+                <form id="navForm" style="display: flex; align-items: center; justify-content: space-between;">  
                     <button type="button" class="nav-link menu-btn">
                         <i class="fas fa-bars"></i>
                         Menu
                     </button>
-                    <a href="<?php echo site_url('welcome/contenu/Accueil');?>">
-                        <button type="button" class="nav-link">
-                            <i class="fas fa-home"></i> Accueil
-                    </button>
-                     
-                    <a href="<?php echo site_url('welcome/contenu/Connexion');?>">
-                        <button type="button" class="nav-link">
-                            <i class="fas fa-user"></i> Compte
-                    </button>
+                    <a href="<?php echo site_url('welcome/contenu/Accueil'); ?>">
+                        <img src="<?php echo base_url('assets/img/Accueil.png'); ?>" alt="Logo" />
                     </a>
-                    
-            
-                    
                 </form>
             </nav>   
         </header>
