345c22c41263da76584be57ce3c52c4c0ad90347
PFISTER Ludovic <ludovicp2005@gmail.com>
2025-04-23 11:26:43 +0200

    CHG Annonces et Création

--- application/views/Annonces.php
+++ application/views/Annonces.php
@@ -10,66 +10,71 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>  
             
             <?php
-            // Inclure le fichier de configuration de la base de données
-            include "application/config/database.php";
+                include "application/config/database.php";
 
-            // Vérifier si l'utilisateur est connecté
-            if (isset($_SESSION['identifiant'])) {
-                // Récupérer le type de compte de l'utilisateur
-                $idCompte = $_SESSION['identifiant'];
-                $selectTypeCompte = "SELECT typeCompte FROM COMPTE WHERE idCompte = :idCompte";
-                $stmt = $pdo->prepare($selectTypeCompte);
-                $stmt->bindParam(':idCompte', $idCompte, PDO::PARAM_STR);
-                $stmt->execute();
-                $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                if ($row['typeCompte'] === 'acheteur') {
-                    // Si l'utilisateur est un acheteur, afficher les annonces
-                    $selectAnnonces = "SELECT idBateau, datePeche, idLot, prixEnchere, heureEnchere, nomAnnonce, idCompteV FROM ANNONCE ORDER BY idLot";
-                    $stmt = $pdo->prepare($selectAnnonces);
+                // Vérifier si l'utilisateur est connecté
+                if (isset($_SESSION['identifiant'])) {
+                    // Récupérer le type de compte de l'utilisateur
+                    $idCompte = $_SESSION['identifiant'];
+                    $selectTypeCompte = "SELECT typeCompte FROM COMPTE WHERE idCompte = :idCompte";
+                    $stmt = $pdo->prepare($selectTypeCompte);
+                    $stmt->bindParam(':idCompte', $idCompte, PDO::PARAM_STR);
                     $stmt->execute();
-                    $rows = $stmt->fetchAll();
-                    
-                    if(count($rows) > 0) {
-                        echo '<table> <!-- table est un tableau -->
-                            <thead> <!-- thead est le haut du tableau -->
-                                <tr> <!-- tr est une ligne dans le tableau -->
-                                    <!-- th est l en-tete du tableau, le nom des colonnes -->
-                                    <th scope="col">Bateau</th>
-                                    <th scope="col">Date de pêche</th>
-                                    <th scope="col">Lot n°</th>
-                                    <th scope="col">Prix</th> 	
-                                    <th scope="col">Heure</th>
-                                    <th scope="col">Titre</th>
-                                    <th scope="col">Vendeur</th>
-                                </tr>
-                            </thead>
-                            <tbody> <!-- tbody est le reste du tableau -->';
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                        foreach ($rows as $row) {
-                            echo '<tr>
-                                <td>'.$row['idBateau'].'</td>
-                                <td>'.$row['datePeche'].'</td>
-                                <td>'.$row['idLot'].'</td>
-                                <td>'.$row['prixEnchere'].'</td>
-                                <td>'.$row['heureEnchere'].'</td>
-                                <td>'.$row['nomAnnonce'].'</td>
-                                <td>'.$row['idCompteV'].'</td>
-                                </tr>';
+                    if ($row['typeCompte'] === 'acheteur') {
+                        // Si l'utilisateur est un acheteur, afficher les annonces
+                        $selectAnnonces = "SELECT idBateau, datePeche, idLot, prixEnchere, heureEnchere, nomAnnonce, idCompteV FROM ANNONCE ORDER BY idLot";
+                        $stmt = $pdo->prepare($selectAnnonces);
+                        $stmt->execute();
+                        $rows = $stmt->fetchAll();
+                        
+                        if(count($rows) > 0) // on vérifie que le nombre d'éléments dans $rows est supérieur à 0, soit que $rows ne soit pas vide
+                        {
+                            echo '<table> <!-- table est un tableau -->
+                                <thead> <!-- thead est le haut du tableau -->
+                                    <tr> <!-- tr est une ligne dans le tableau -->
+                                        <!-- th est l en-tete du tableau, le nom des colonnes -->
+                                        <th scope="col">Bateau</th>
+                                        <th scope="col">Date de pêche</th>
+                                        <th scope="col">Lot n°</th>
+                                        <th scope="col">Prix</th> 	
+                                        <th scope="col">Heure</th>
+                                        <th scope="col">Titre</th>
+                                        <th scope="col">Vendeur</th>
+                                    </tr>
+                                </thead>
+                                <tbody> <!-- tbody est le reste du tableau -->';
+
+                            foreach ($rows as $row) 
+                            {
+                                echo '<tr>
+                                    <td>'.$row['idBateau'].'</td>
+                                    <td>'.$row['datePeche'].'</td>
+                                    <td>'.$row['idLot'].'</td>
+                                    <td>'.$row['prixEnchere'].'</td>
+                                    <td>'.$row['heureEnchere'].'</td>
+                                    <td>'.$row['nomAnnonce'].'</td>
+                                    <td>'.$row['idCompteV'].'</td>
+                                    </tr>';
+                            }
+                            echo '
+                                </tbody>
+                            </table>';
+                        } else {
+                            echo "Aucune annonce n'est disponible pour le moment.";
+                            // si $rows est vide, cela signifie qu'aucune annonce n'a été trouvée ou n'existe dans la base
                         }
-                        echo '</tbody></table>';
-                    } else {
-                        echo "Aucune annonce n'est disponible pour le moment.";
+                    } elseif ($row['typeCompte'] === 'vendeur') {
+                        // Si l'utilisateur est un vendeur, afficher le bouton pour créer une annonce
+                        echo '<form method="POST" action="' . site_url('welcome/contenu/Annonces_Creation') . '">
+                            <button class="btn">Créer mon annonce</button>
+                        </form>';
                     }
-                } elseif ($row['typeCompte'] === 'vendeur') {
-                    // Si l'utilisateur est un vendeur, afficher le bouton pour créer une annonce
-                    echo '<form method="POST" action="' . site_url('welcome/contenu/Annonces_Creation') . '">
-                        <button class="btn">Créer mon annonce</button>
-                    </form>';
+                } else {
+                    echo "Veuillez vous connecter pour voir les annonces ou créer une annonce.";
                 }
-            } else {
-                echo "Veuillez vous connecter pour voir les annonces ou créer une annonce.";
-            }
+                $pdo=null; // on ferme la connexion à la base de données en donnant une valeur vide à $pdo
             ?>
         </div>
     </section> 
--- application/views/Annonces_Creation.php
+++ application/views/Annonces_Creation.php
@@ -10,12 +10,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<br>
         
         <form method="POST" action="<?php echo site_url('welcome/annonces_traitement'); ?>">
+            <?php	
+                include "application/config/database.php";
+            
+                $selectBateaux = "SELECT idBateau, immatriculation FROM BATEAU";                
+                $stmt = $pdo->prepare($selectBateaux);
+                $stmt->execute();
+                $rows = $stmt->fetchAll();
+            
+                if(count($rows) > 0) // on vérifie que le nombre d'éléments dans $rows est supérieur à 0, soit que $rows ne soit pas vide
+                {
+                    echo '
+                    <label for="idBateau">Bateau :</label><br>
+                    <select id="idBateau" name="idBateau" required>';
+                        foreach ($rows as $row) // boucle pour tous les éléments dans $rows
+                        { 
+                            echo '<option value='.$row['idBateau'].'>'.$row['idBateau'].' - '.$row['immatriculation'].'</option>';
+                        }
+                    echo '</select><br><br>';
+                    
+                } else {
+                    echo "Erreur : aucun enregistrement de bateau trouvé";
+                    // si $rows est vide, cela signifie qu'aucun bateau n'existe dans la base
+                }
+                $pdo=null; // on ferme la connexion à la base de données en donnant une valeur vide à $pdo
+            ?>
 
-            <label for="typeCompte">Bateau :</label><br>
-            <select id="typeCompte" name="typeCompte" required>
-                <option value="acheteur">Acheteur</option>
-                <option value="vendeur">Vendeur</option>
-            </select><br>
+            <label for="datePeche">Date :</label><br>
+            <input type="date" id="datePeche" name="datePeche" required><br>
+
+            <label for="prixEnchere">Prix :</label><br>
+            <input type="number" min="1" step="any" id="prixEnchere" name="prixEnchere" required><br>
+
+            <label for="heureEnchere">Heure :</label><br>
+            <input type="time" id="heureEnchere" name="heureEnchere" required><br>
+
+            <label for="nomAnnonce">Titre :</label><br>
+            <input type="text" id="nomAnnonce" name="nomAnnonce" required><br>
 
 			<br>
             <button type="submit" class="btn">Valider</button>
